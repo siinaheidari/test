56e68b6962c1bb08beab9cae07ff6639d16f557d
sina <kokibernard123@gmail.com>
2024-05-29 13:24:49 -0700

    bullgav 5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import {Poppins} from "next/font/google";
 import "./globals.css";
 import Providers from "@/app/providers";
 import {Layout} from "antd";
-
+import Head from 'next/head'
 
 const poppins = Poppins({subsets: ["latin"], weight: ["300", "400", "500", "600", "700"]});
 
@@ -17,9 +17,9 @@ export default function RootLayout({children,}: Readonly<{
 }>) {
     return (
         <html lang="en">
-        <head>
+        <Head>
             <meta name="viewport" content="width=device-width, height=device-height,  initial-scale=1.0, user-scalable=no;user-scalable=0;"/>
-        </head>
+        </Head>
         <body className={poppins.className + " !min-h-[calc(100dvh)]"}>
         <Providers>
             <Layout className={'!min-h-[calc(100dvh)] text-white'}>
