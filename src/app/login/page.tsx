f1b302572d6f0c4b1b8b8dabeddf8f8c7e777355
sina <kokibernard123@gmail.com>
2024-06-01 09:17:38 -0700

    bullgav 19

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@
 import React, {FC, useState} from 'react';
 import BottomSheet from "@/app/components/bottom-sheet";
 import {Controller, useForm} from "react-hook-form";
-import {Button, Input, Modal, Radio} from "antd";
+import {Button, Input, Modal, Radio, Spin} from "antd";
 import {UserOutlined} from "@ant-design/icons";
 import musicLottie from "../../../public/assets/lottie/music.json"
 import downLottie from "../../../public/assets/lottie/down.json"
@@ -11,152 +11,158 @@ import {ReactTyped} from "react-typed";
 
 
 type TForm = {
-    name: string,
-    password: string
-    gender: number
+  name: string,
+  password: string
+  gender: number
 }
 
 const AboutPage: FC = () => {
-    const [open, setOpen] = useState(false);
-    const {register, handleSubmit, formState: {errors}, control, watch} = useForm<TForm>()
-    const [openModal, setOpenModal] = useState(0)
-    const genderWatch: number = watch("gender")
+  const [open, setOpen] = useState(false);
+  const {register, handleSubmit, formState: {errors}, control, watch} = useForm<TForm>()
+  const [openModal, setOpenModal] = useState(0)
+  const genderWatch: number = watch("gender")
+  const [loadingMosa, setLoadingMosa] = useState(true)
+  const [loadingParsa, setLoadingParsa] = useState(true)
+  const handleSubmitForm = () => {
+    setOpen(false)
+    setOpenModal(genderWatch)
+    setTimeout(() => {
+      setLoadingMosa(false)
+    }, 2000)
+    setTimeout(() => {
+      setLoadingParsa(false)
+    }, 2000)
+  }
 
-    const handleSubmitForm = () => {
-        setOpen(false)
-        setOpenModal(genderWatch)
-    }
 
+  return (
+    <div>
+      <div className={"mt-[9rem]"}>
+        {!open && <Lottie animationData={downLottie} className={"max-w-[400px] mx-auto"}/>}
+      </div>
 
-    return (
-        <div>
-            <div className={"mt-[9rem]"}>
-                {!open && <Lottie animationData={downLottie} className={"max-w-[400px] mx-auto"}/> }
+      <BottomSheet
+        onClose={() => setOpen(false)}
+        isOpen={open}
+        className={"[&>div]:!bg-white/99 [&>div]:overflow-auto"}
+        snapPoints={[740, 400, 0]}
+      >
+        <div className={"relative h-full"}>
+          <div className={"text-[30px] text-center font-bold text-[#303030]"}>
+            <ReactTyped strings={["Login"]} typeSpeed={80}/>
+          </div>
+          <div className={"text-[40px] text-center font-bold text-[#A6031A]"}>
+            <Lottie animationData={musicLottie} className={"size-[290px] mx-auto"}/>
+          </div>
+          <form onSubmit={handleSubmit(handleSubmitForm)} className={"space-y-1"} autoComplete="nope">
+            <div>
+              <label>name :</label>
+              <div>
+                <Controller<TForm>
+                  control={control}
+                  name="name"
+                  rules={{required: "name is required"}}
+                  render={({field, fieldState}) => (
+                    <Input
+                      placeholder={"name"}
+                      autoComplete="new-sssss"
+                      {...field}
+                      suffix={<UserOutlined className={fieldState.invalid ? 'icon-error' : ''}/>}
+                      className={fieldState.invalid ? 'error' : 'custom-input'}
+                      // onChange={e => console.log(e.target.value)}
+                    />
+                  )}
+                />
+                {errors.name && <p className='error-message'>{errors.name.message}</p>}
+              </div>
+            </div>
+            <div>
+              <label>password :</label>
+              <div>
+                <Controller<TForm>
+                  name="password"
+                  control={control}
+                  rules={{required: "password is required"}}
+                  render={({field, fieldState}) => (
+                    <Input.Password
+                      placeholder={"password"}
+                      {...field}
+                      autoComplete="new-password"
+                      className={fieldState.invalid ? 'error' : 'custom-input'}
+                    />
+                  )}
+                />
+                {errors.password && (<p className='error-message'>{errors.password.message}</p>)}
+              </div>
+            </div>
+            <div>
+              <label>gender :</label>
+              <div>
+                <Controller<TForm>
+                  name="gender"
+                  control={control}
+                  rules={{required: "gender is required"}}
+                  render={({field, fieldState}) => (
+                    <Radio.Group {...field}>
+                      <Radio value={1}>female</Radio>
+                      <Radio value={2}>male</Radio>
+
+                    </Radio.Group>
+                  )}
+                />
+                {errors.gender && (<p className='error-message'>{errors.gender.message}</p>)}
+              </div>
             </div>
 
-
-            <video preload={"auto"} className={"bg-cover object-cover !h-screen !size-full hidden"} autoPlay muted
-                   playsInline loop
-                   controls={false}>
-                <source src="https://biaupload.com/static/files-2024-05/org-8cee627bfeee2.mp4"
-                        type="video/mp4"/>
-            </video>
-            <video preload={"auto"} className={"bg-cover object-cover !h-screen !size-full hidden"} autoPlay muted
-                   playsInline loop
-                   controls={false}>
-                <source src="https://biaupload.com/static/files-2024-05/org-d318afe942651.mp4"
-                        type="video/mp4"/>
-            </video>
-
-
-            <BottomSheet
-                onClose={() => setOpen(false)}
-                isOpen={open}
-                className={"[&>div]:!bg-white/99"}
-                snapPoints={[740, 400, 0]}
-            >
-                <div className={"relative h-full"}>
-                    <div className={"text-[30px] text-center font-bold text-[#303030]"}>
-                        <ReactTyped strings={["Login"]} typeSpeed={80}/>
-                    </div>
-                    <div className={"text-[40px] text-center font-bold text-[#A6031A]"}>
-                        <Lottie animationData={musicLottie} className={"size-[290px] mx-auto"}/>
-                    </div>
-                    <form onSubmit={handleSubmit(handleSubmitForm)} className={"space-y-1"} autoComplete="nope">
-                        <div>
-                            <label>name :</label>
-                            <div>
-                                <Controller<TForm>
-                                    control={control}
-                                    name="name"
-                                    rules={{required: "name is required"}}
-                                    render={({field, fieldState}) => (
-                                        <Input
-                                            placeholder={"name"}
-                                            autoComplete="new-sssss"
-                                            {...field}
-                                            suffix={<UserOutlined className={fieldState.invalid ? 'icon-error' : ''}/>}
-                                            className={fieldState.invalid ? 'error' : 'custom-input'}
-                                            // onChange={e => console.log(e.target.value)}
-                                        />
-                                    )}
-                                />
-                                {errors.name && <p className='error-message'>{errors.name.message}</p>}
-                            </div>
-                        </div>
-                        <div>
-                            <label>password :</label>
-                            <div>
-                                <Controller<TForm>
-                                    name="password"
-                                    control={control}
-                                    rules={{required: "password is required"}}
-                                    render={({field, fieldState}) => (
-                                        <Input.Password
-                                            placeholder={"password"}
-                                            {...field}
-                                            autoComplete="new-password"
-                                            className={fieldState.invalid ? 'error' : 'custom-input'}
-                                        />
-                                    )}
-                                />
-                                {errors.password && (<p className='error-message'>{errors.password.message}</p>)}
-                            </div>
-                        </div>
-                        <div>
-                            <label>gender :</label>
-                            <div>
-                                <Controller<TForm>
-                                    name="gender"
-                                    control={control}
-                                    rules={{required: "gender is required"}}
-                                    render={({field, fieldState}) => (
-                                        <Radio.Group {...field}>
-                                            <Radio value={1}>female</Radio>
-                                            <Radio value={2}>male</Radio>
-
-                                        </Radio.Group>
-                                    )}
-                                />
-                                {errors.gender && (<p className='error-message'>{errors.gender.message}</p>)}
-                            </div>
-                        </div>
-
-                        <div className={"absolute bottom-[2rem] w-full"}>
-                            <Button htmlType={"submit"} type={"primary"} block>
-                                submit
-                            </Button>
-                        </div>
-                    </form>
-
-                </div>
-            </BottomSheet>
-
-            <div className={`${open === true ? "hidden" : ""} absolute bottom-[2rem] w-full px-3`}>
-                <Button onClick={() => setOpen(true)} block>
-                    login</Button>
+            <div className={"absolute bottom-[1rem] w-full py-2"}>
+              <Button htmlType={"submit"} type={"primary"} block>
+                submit
+              </Button>
             </div>
-
-            <Modal destroyOnClose open={openModal === 1} footer={false} onCancel={() => setOpenModal(0)}>
-                <video preload={"auto"} className={" !h-[500px] !size-full"} autoPlay playsInline
-                       controls={false}>
-                    <source src={"https://biaupload.com/static/files-2024-05/org-8cee627bfeee2.mp4"}
-                            type="video/mp4"/>
-                </video>
-            </Modal>
-
-
-            <Modal className={"!p-0"} destroyOnClose open={openModal === 2} footer={false}
-                   onCancel={() => setOpenModal(0)}>
-                <video preload={"auto"} className={"bg-cover object-cover mx-auto !h-[500px]"} autoPlay playsInline
-                       controls={false}>
-                    <source src="https://biaupload.com/static/files-2024-05/org-d318afe942651.mp4"
-                            type="video/mp4"/>
-                </video>
-            </Modal>
+          </form>
 
         </div>
-    );
+      </BottomSheet>
+
+      <div className={`${open === true ? "hidden" : ""} absolute bottom-[2rem] w-full px-3`}>
+        <Button onClick={() => setOpen(true)} block>
+          login</Button>
+      </div>
+
+      <Modal destroyOnClose open={openModal === 1} footer={false} onCancel={() => setOpenModal(0)}>
+
+        {
+          loadingMosa ?
+            <Spin spinning={loadingMosa}>
+
+            </Spin>
+            : <video preload={"auto"} className={" !h-[500px] !size-full"} autoPlay playsInline
+                     controls={false}>
+              <source src={"https://biaupload.com/static/files-2024-05/org-8cee627bfeee2.mp4"}
+                      type="video/mp4"/>
+            </video>
+        }
+
+      </Modal>
+
+      <Modal className={"!p-0"} destroyOnClose open={openModal === 2} footer={false}
+             onCancel={() => setOpenModal(0)}>
+        {
+
+          loadingParsa ?
+            <Spin spinning={loadingParsa}>
+            </Spin> :
+            <video preload={"auto"} className={"bg-cover object-cover mx-auto !h-[500px]"} autoPlay playsInline
+                   controls={false}>
+              <source src="https://biaupload.com/static/files-2024-05/org-d318afe942651.mp4"
+                      type="video/mp4"/>
+            </video>
+        }
+
+      </Modal>
+
+    </div>
+  );
 };
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,35 +2,44 @@
 
 import {FC, useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
+import {Spin} from "antd";
 
 const HomePage: FC = () => {
 
-    const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
 
-    const router = useRouter()
-
-    useEffect(() => {
-        setTimeout(() => {
-            router.push("/login")
-        }, 5300)
-    }, [])
+  const router = useRouter()
 
 
+  useEffect(() => {
+    if (loading) {
+      setTimeout(()=>{
+        setLoading(false)
+      },2500)
+    } else setTimeout(() => {
+      router.push("/login")
+    }, 5000)
+  }, [loading])
 
 
-    return (
-        <div className='mx-auto center  !min-h-screen'>
+  return (
+    <div className='mx-auto center !min-h-screen'>
+      {
+        loading ?
+          <Spin spinning={loading}>
+
+          </Spin>
+          : <div>
+            <video preload={"auto"} className={"bg-cover object-cover !h-screen !size-full"} autoPlay muted playsInline
+                   controls={false}>
+              <source src="https://biaupload.com/static/files-2024-06/org-ba07c2005f481.mp4" type="video/mp4"/>
+            </video>
+          </div>
+      }
 
 
-            <div>
-                <video preload={"auto"} className={"bg-cover object-cover !h-screen !size-full"}  autoPlay muted playsInline loop
-                       controls={false}>
-                    <source src="https://biaupload.com/static/files-2024-05/org-409c051ff8533.mp4" type="video/mp4"/>
-                </video>
-            </div>
-
-        </div>
-    );
+    </div>
+  );
 };
 
 export default HomePage;
