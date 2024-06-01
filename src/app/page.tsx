9da34a5949a352e948736b7f9de64bfe8bab4e12
sina <kokibernard123@gmail.com>
2024-06-01 14:53:25 -0700

    bullgav 31

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 "use client"
 
-import {FC, useEffect, useState} from "react";
+import {FC, useState} from "react";
 import {useRouter} from "next/navigation";
 import {Spin} from "antd";
-import ReactPlayer from "react-player";
 
 const HomePage: FC = () => {
 
   const [loading, setLoading] = useState(true)
   const router = useRouter()
+
+  setTimeout(() => {
+    setLoading(false)
+  }, 2000)
 
 
   return (
@@ -16,20 +19,15 @@ const HomePage: FC = () => {
 
       <Spin spinning={loading}>
         <div>
-          <ReactPlayer url='https://biaupload.com/static/files-2024-06/org-ba07c2005f481.mp4'
-                       className={" !bg-cover !object-cover !h-screen !size-full overflow-hidden !bg-black"}
-                       playing={true}
-                       controls={false}
-                       playsinline={true}
-                       autoPlay={true}
-                       muted
-                       onReady={() => {
-                         setLoading(false)
-                       }}
-                       onEnded={() => {
-                         router.push("/login")
-                       }}
-          />
+          <video
+
+            onEnded={() => {
+              router.push("/login")
+            }}
+            preload={"auto"} className={"bg-cover object-cover !h-screen !size-full"} autoPlay muted playsInline
+            controls={false}>
+            <source src="https://biaupload.com/static/files-2024-06/org-ba07c2005f481.mp4" type="video/mp4"/>
+          </video>
         </div>
 
       </Spin>
