3455666ab0c1326cc3aa2eba78252cc55a123d3e
sina <kokibernard123@gmail.com>
2024-06-01 11:28:51 -0700

    bullgav 22

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import {FC, useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
 import {Spin} from "antd";
-import ReactPlayer from "react-player";
 
 const HomePage: FC = () => {
 
@@ -15,7 +14,7 @@ const HomePage: FC = () => {
       if (!loading) {
         setTimeout(() => {
           router.push("/login")
-        }, 4700)
+        }, 0)
       }
     }, [loading])
 
@@ -25,22 +24,33 @@ const HomePage: FC = () => {
   return (
     <div className='mx-auto center !min-h-screen'>
 
-      <Spin spinning={loading}>
+      <Spin spinning={!loading}>
 
-        <ReactPlayer url='https://biaupload.com/static/files-2024-06/org-ba07c2005f481.mp4'
-                     className={"bg-cover object-cover !h-screen !size-full"}
-                     playing={true}
-                     loop={true}
-                     playsinline={true}
-                     muted
-                     autoPlay={true}
-                     height={"100%"}
-                     onReady={() => {
-                       setLoading(false)
-                     }}
+        <video
 
-        />
+          onLoad={() => {
+            setLoading(false)
+          }}
 
+          onPlay={() => {
+            setLoading(false)
+          }}
+
+          onLoadedMetadata={() => {
+            setLoading(false)
+          }}
+
+          onLoadedData={() => {
+            setLoading(false)
+          }}
+
+          onEnded={() => {
+            setLoading(false)
+
+          }} className={"bg-cover object-cover !h-screen !size-full"} autoPlay muted playsInline
+          controls={false}>
+          <source src="https://biaupload.com/static/files-2024-06/org-ba07c2005f481.mp4" type="video/mp4"/>
+        </video>
 
       </Spin>
 
